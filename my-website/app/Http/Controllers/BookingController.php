4c2c4af90c75f236c88c1b89dc5b777632d4c714
Twiskersmoose <58926161+Twiskersmoose@users.noreply.github.com>
2026-05-15 10:12:54 +1000

    TC

--- my-website/app/Http/Controllers/BookingController.php
+++ my-website/app/Http/Controllers/BookingController.php
@@ -4,35 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    // this is all PHP
-
-    public function index(){
-        $test = 1 + 1;
-        $testArr = [
-            "var_A" => 1234567,
-            "var_B" => $this->Calc(1,4)
-        ];
-
-        return view('home.home',["test_var" => $testArr]);
-    }
-
-    public function load_rooms(){
-
-        return view('rooms.rooms');
-    }
-    
-
-    public function Calc($number1,$number2){
-        return $number1 + $number2;
-    }
-
-
-}
-
 class BookingController extends Controller {
-public function load_book(){
+    public function load_book(){
         $room_1_array = [
             "name" => 'Deluxe Room',
             "available_from" => '2026-07-01',
