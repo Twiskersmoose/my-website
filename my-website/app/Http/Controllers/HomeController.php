978b133a2e0d74f24717139017c57201c1816658
Twiskersmoose <58926161+Twiskersmoose@users.noreply.github.com>
2026-05-15 10:13:01 +1000

    Update HomeController.php

--- my-website/app/Http/Controllers/HomeController.php
+++ my-website/app/Http/Controllers/HomeController.php
@@ -31,15 +31,3 @@ class HomeController extends Controller
 
 }
 
-class BookingController extends Controller {
-public function load_book(){
-        $room_1_array = [
-            "name" => 'Deluxe Room',
-            "available_from" => '2026-07-01',
-            "bookings" => [1 => ["start" => '2026-07-05', "end" => '2026-07-09'],
-                          2 => ["start" => '2026-07-16', "end" => '2026-07-21']],
-            "max_booking_length" => 7
-        ];
-        return view('rooms.book',["room_array" => $room_1_array]);
-    }
-}
